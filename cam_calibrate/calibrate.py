afbe9f800731b45927cfc3ea46fbc37791723c2a
BatmanofZuhandArrgh <anhnguyen199819981998@gmail.com>
2023-11-22 15:30:48 +0800

    calibration

---
+++ cam_calibrate/calibrate.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cv2 as cv
+import glob
+import pickle
+import os
+
+def save_mat(array,outfile = 'cam_calibrate/intrinsic_mat.pkl'):
+    with open(outfile, 'wb') as pickle_file:
+        pickle.dump(array, pickle_file)
+
+def load_mat(outfile):
+    with open(outfile, 'rb') as pickle_file:
+        loaded_data = pickle.load(pickle_file)
+    return loaded_data
+    
+
+CHECKERBOARD = (7,6)
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((1, CHECKERBOARD[0]*CHECKERBOARD[1],3), np.float32)
+objp[0, :,:2] = np.mgrid[0:CHECKERBOARD[0],0:CHECKERBOARD[1]].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+
+images = glob.glob('cam_calibrate/calib_img/*.png')
+
+for fname in images:
+    img = cv.imread(fname)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, CHECKERBOARD, cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+        # Draw and display the corners
+        cv.drawChessboardCorners(img, CHECKERBOARD, corners2, ret)
+        cv.imshow('img', img)
+        cv.imwrite(os.path.dirname(fname) + '/' + os.path.basename(fname).split('.')[0]+ 'calib.png', img)
+        cv.waitKey(500)
+cv.destroyAllWindows()
+
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+out_intrinsic = 'cam_calibrate/intrinsic_mat.pkl'
+out_extrinsic = 'cam_calibrate/extrinsic_mat.pkl'
+
+
+save_mat(mtx)
+intrinsic_mat = load_mat(out_intrinsic)
+print(intrinsic_mat)
+
+print(rvecs)
+print(tvecs)
+
+# save_mat()
+
+# Choose an index for the calibration image you are interested in
+calibration_image_index = 0
+
+# Get the rotation vector and translation vector for the chosen image
+rvec = rvecs[calibration_image_index]
+tvec = tvecs[calibration_image_index]
+
+# Convert the rotation vector to a rotation matrix
+R, _ = cv.Rodrigues(rvec)
+
+# Construct the extrinsic matrix [R|t]
+extrinsic_matrix = np.hstack((R, tvec))
+
+print("Extrinsic Matrix:")
+print(extrinsic_matrix)
+save_mat(extrinsic_matrix, outfile=out_extrinsic)
---
+++ cam_calibrate/get_images.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+def capture_images(outfolder = 'cam_calibrate/calib_img'):
+    # Open the webcam
+    cap = cv2.VideoCapture(0)  # 0 represents the default camera (you may need to change it if you have multiple cameras)
+    index = 0
+    while True:
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Display the frame
+        cv2.imshow('Webcam', frame)
+        # print('HEYO')
+        # Check for the 'ESC' key to exit the loop
+        k = cv2.waitKey(1)%256
+        if k  == 27:
+            break
+
+        elif k ==  32:  # 32 is the ASCII code for the space key
+            # Save the captured frame as an image
+            image_filename = os.path.join(outfolder, str(index)+'.png')
+            print(image_filename)
+            cv2.imwrite(image_filename, frame)
+            print(f"Image captured and saved as {image_filename}")
+            
+        index+=1 
+
+    # Release the webcam and close the window
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Run the function
+capture_images()
+
